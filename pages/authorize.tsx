f990044856a1e2e494e33e7f1c3a03974138d314
Matthew Stanciu <mattbstanciu@gmail.com>
2024-02-11 14:56:23 -0500

    make initial form better

--- pages/authorize.tsx
+++ pages/authorize.tsx
@@ -10,12 +10,15 @@ export default function Authorize() {
   const [passport, setPassport] = useState("");
   const [state, setState] = useState(AuthState.EnterNumber);
   const [totpNeeded, setTotpNeeded] = useState(false);
+  const [numberFormPending, setNumberFormPending] = useState(false);
 
   const id = passport.includes(".")
     ? parseInt(passport.split(".")[1] ?? "0")
     : Number(passport);
 
-  const onChoosePassport = async () => {
+  const selectPassport = async () => {
+    setNumberFormPending(true);
+
     // Send a request to initiate lock
     const res = await fetch("/api/scan", {
       method: "POST",
@@ -75,7 +78,13 @@ export default function Authorize() {
       {state == AuthState.EnterNumber && (
         <div className="flex flex-col items-center gap-2">
           <p className="font-bold text-2xl">Enter passport number</p>
-          <div className="flex flex-row gap-2">
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              selectPassport();
+            }}
+            className="flex flex-row gap-2"
+          >
             <input
               className="border-2 border-black w-24 p-1 rounded-sm font-mono"
               type="number"
@@ -87,16 +96,15 @@ export default function Authorize() {
             />
             <button
               className="py-1 px-2 font-bold bg-amber-400 hover:bg-amber-500 transition duration-100 border-2 border-black shadow-blocks-tiny disabled:bg-gray-300"
-              onClick={() => {
-                onChoosePassport();
-              }}
               disabled={
-                passport.length === 0 || !/^(?:\d\.)?(\d{1,4})$/.test(passport)
+                passport.length === 0 ||
+                !/^(?:\d\.)?(\d{1,4})$/.test(passport) ||
+                numberFormPending
               }
             >
-              Submit
+              {numberFormPending ? "Submitting..." : "Submit"}
             </button>
-          </div>
+          </form>
         </div>
       )}
       {state == AuthState.WaitForScan && (
