3fa250d1a5930972adb7ce59e1c4de54ca1ae209
Jack Hogan <jackhogan11@gmail.com>
2024-03-02 16:15:29 -0500

    Updated landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,11 @@
-import Link from "next/link";
-
 export default function Index() {
   return (
     <div className="p-4 max-w-lg">
       <h1 className="text-2xl font-bold font-mono">id.purduehackers.com</h1>
       <p>
-        this is purdue hackers&rsquo; passport-based authentication service.
-        WIP, please do not use unless you&rsquo;ve talked with jack or matthew.
+        This is purdue hackers&rsquo; passport-based authentication service.
+        You can't do very much here. Try using Sign In with Passport on a supported page.
       </p>
-      <Link href="/authorize" className="underline">
-        get started here
-      </Link>
     </div>
   );
 }
