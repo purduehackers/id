fd04bbbecf18085d1764cafcc487495f4fdbaa0b
Jack Hogan <jackhogan11@gmail.com>
2024-08-05 15:29:55 -0400

    Removed print

--- pages/authorize.tsx
+++ pages/authorize.tsx
@@ -19,7 +19,6 @@ export default function Authorize({
   const searchParams = useSearchParams();
   const clientId = searchParams.get("client_id");
   const scopes = (searchParams.get("scope") ?? "").split(" ");
-  console.log(scopes);
 
   const [passportNumber, setPassportNumber] = useState("");
   const [authState, setAuthState] = useState(
