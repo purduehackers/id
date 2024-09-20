ba0f9d3eda8e8baa094b314b1ecc448a8ec63f5a
Matthew Stanciu <mattbstanciu@gmail.com>
2024-09-20 16:29:23 -0400

    fmt

--- api/user.rs
+++ api/user.rs
@@ -1,13 +1,8 @@
-use id::{wrap_error, oauth_user, db};
-use serde::{
-    Serialize,
-    Deserialize
-};
-use vercel_runtime::{run, Body, Error, Request, Response};
-use entity::{
-    passport, prelude::*, sea_orm_active_enums::RoleEnum, user
-};
+use entity::{passport, prelude::*, sea_orm_active_enums::RoleEnum, user};
+use id::{db, oauth_user, wrap_error};
 use sea_orm::prelude::*;
+use serde::{Deserialize, Serialize};
+use vercel_runtime::{run, Body, Error, Request, Response};
 
 #[derive(Serialize, Deserialize)]
 struct UserWithPassport {
@@ -28,17 +23,25 @@ pub async fn handler(req: Request) -> Result<Response<Body>, Error> {
 
     let db = db().await?;
 
-    let user = User::find().filter(user::Column::Id.eq(user_id)).one(&db).await?.ok_or_else(|| Error::from("User not found"))?;
-    let latest_passport = Passport::find().filter(passport::Column::OwnerId.eq(user_id)).one(&db).await?;
+    let user = User::find()
+        .filter(user::Column::Id.eq(user_id))
+        .one(&db)
+        .await?
+        .ok_or_else(|| Error::from("User not found"))?;
+    let latest_passport = Passport::find()
+        .filter(passport::Column::OwnerId.eq(user_id))
+        .one(&db)
+        .await?;
 
     let response_data = UserWithPassport {
         id: user.id,
         discord_id: user.discord_id.clone(),
         role: user.role.clone(),
         totp: user.totp.clone(),
-        latest_passport: latest_passport.clone()
+        latest_passport: latest_passport.clone(),
     };
 
-    Ok(Response::new(Body::Text(serde_json::to_string(&response_data)?)))
+    Ok(Response::new(Body::Text(serde_json::to_string(
+        &response_data,
+    )?)))
 }
-
