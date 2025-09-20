a0a955dcea2ec3207b803d80c8a7ac94e52c5bf6
Jack Hogan <jackhogan11@gmail.com>
2025-09-20 18:38:33 -0400

    Fix tailwind

--- src/app.rs
+++ src/app.rs
@@ -1,25 +1,25 @@
 use leptos::prelude::*;
-use leptos_meta::{MetaTags, Stylesheet, Title, provide_meta_context};
+use leptos_meta::{provide_meta_context, MetaTags, Stylesheet, Title};
 use leptos_router::{
-    StaticSegment,
     components::{Route, Router, Routes},
+    StaticSegment,
 };
 
-use crate::pages::{Index, authorize::Authorize, scan::Scan};
+use crate::pages::{authorize::Authorize, scan::Scan, Index};
 
 pub fn shell(options: LeptosOptions) -> impl IntoView {
     view! {
         <!DOCTYPE html>
         <html lang="en">
             <head>
-                <meta charset="utf-8"/>
-                <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                <meta charset="utf-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <AutoReload options=options.clone() />
-                <HydrationScripts options/>
-                <MetaTags/>
+                <HydrationScripts options />
+                <MetaTags />
             </head>
             <body>
-                <App/>
+                <App />
             </body>
         </html>
     }
@@ -33,18 +33,18 @@ pub fn App() -> impl IntoView {
     view! {
         // injects a stylesheet into the document <head>
         // id=leptos means cargo-leptos will hot-reload this stylesheet
-        <Stylesheet id="leptos" href="/pkg/{{project-name}}.css"/>
+        <Stylesheet id="leptos" href="/pkg/id.css" />
 
         // sets the document title
-        <Title text="ID"/>
+        <Title text="ID" />
 
         // content for this welcome page
         <Router>
             <main>
                 <Routes fallback=|| "Page not found.".into_view()>
-                    <Route path=StaticSegment("") view=Index/>
-                    <Route path=StaticSegment("authorize") view=Authorize/>
-                    <Route path=StaticSegment("scan") view=Scan/>
+                    <Route path=StaticSegment("") view=Index />
+                    <Route path=StaticSegment("authorize") view=Authorize />
+                    <Route path=StaticSegment("scan") view=Scan />
                 </Routes>
             </main>
         </Router>
