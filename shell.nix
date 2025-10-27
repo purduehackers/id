ba0def620e74f5147f2b8d21a90a807b7bc7540c
Jack Hogan <jackhogan11@gmail.com>
2025-10-27 11:53:58 -0400

    Remove secret for user endpoint

--- shell.nix
+++ shell.nix
@@ -1,8 +1,6 @@
 { pkgs ? import <nixpkgs> {} }:
 pkgs.mkShell {
     nativeBuildInputs = with pkgs.buildPackages; [
-        nodePackages.pnpm
-        nodePackages.prisma
-        nodejs_22
+        
     ];
 }
--- src/routes/user.rs
+++ src/routes/user.rs
@@ -1,20 +1,31 @@
-use axum::{Json, extract::State};
+use axum::{extract::State, Json};
 use entity::{passport, prelude::*, sea_orm_active_enums::RoleEnum, user};
 use oxide_auth::frontends::simple::endpoint::Vacant;
-use sea_orm::{QueryOrder, prelude::*};
-use serde::{Deserialize, Serialize};
+use sea_orm::{prelude::*, QueryOrder};
+use serde::Serialize;
 
-use crate::routes::{OAuthUser, RouteError, RouteState, scope::USER_READ};
+use crate::routes::{scope::USER_READ, OAuthUser, RouteError, RouteState};
 
-#[derive(Serialize, Deserialize)]
-pub struct UserWithPassport {
+#[derive(Serialize)]
+struct UserWithPassport {
     iss: String,
     sub: i32,
     id: i32,
     discord_id: i64,
     role: RoleEnum,
     totp: Option<String>,
-    latest_passport: Option<passport::Model>,
+    latest_passport: Option<PublicPassport>,
+}
+
+#[derive(Serialize)]
+struct PublicPassport {
+    pub id: i32,
+    pub version: i32,
+    pub surname: String,
+    pub name: String,
+    pub date_of_birth: Date,
+    pub date_of_issue: Date,
+    pub place_of_origin: String,
 }
 
 pub async fn handler(
@@ -39,7 +50,26 @@ pub async fn handler(
         discord_id: user.discord_id,
         role: user.role.clone(),
         totp: user.totp.clone(),
-        latest_passport: latest_passport.clone(),
+        latest_passport: latest_passport.map(
+            |entity::passport::Model {
+                 id,
+                 version,
+                 surname,
+                 name,
+                 date_of_birth,
+                 date_of_issue,
+                 place_of_origin,
+                 ..
+             }| PublicPassport {
+                id,
+                version,
+                surname,
+                name,
+                date_of_birth,
+                date_of_issue,
+                place_of_origin,
+            },
+        ),
     };
 
     Ok(Json(response_data))
